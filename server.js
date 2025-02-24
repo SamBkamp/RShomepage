d2cd08a11fc0229e81e07d76d94f57753c17266a
Sam Bonnekamp <s.bonnekamp@exotic-aqua.com>
2025-02-24 17:11:34 +0800

    implemented favicon

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const PORT = 3000;
 const express = require("express");
 const path = require("path");
 const router = require("./controllers/router.js");
-
+var favicon = require('serve-favicon');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
+app.use(favicon(path.join(__dirname,'public','media','favicon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
