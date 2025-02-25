bef9c01100a749cd2c41587588dd6f9562b0c8b7
Ubuntu <ubuntu@ip-172-31-44-55.ap-southeast-1.compute.internal>
2025-02-25 04:44:51 +0000

    added more views

--- controllers/logic.js
+++ controllers/logic.js
@@ -4,4 +4,19 @@ var root = async (req, res) => {
     res.render("index");
 };
 
+var customs = async (req, res) => {
+    res.render("customs");
+}
+
+var generic = async (req, res) => {
+    res.render("generic");
+}
+
+var tanks = async (req, res) => {
+    res.render("tanks");
+}
+
 exports.root = root;
+exports.customs = customs;
+exports.generic = generic;
+exports.tanks = tanks;
--- controllers/router.js
+++ controllers/router.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const logic = require("./logic.js");
 
 router.get("/", logic.root);
+router.get("/customs", logic.customs);
+router.get("/generic", logic.generic);
+router.get("/tanks", logic.tanks);
 
 module.exports = router;
